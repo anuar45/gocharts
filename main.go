66e24c44b3c642f2d9831d7f15259f9d08dfeaeb
Anuar Serdaliyev <serdaliyev.anuar@gmail.com>
2020-02-09 23:02:24 +0100

    gomod parse ready

--- main.go
+++ main.go
@@ -1,73 +1,111 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"net/url"
+	"os"
 	"regexp"
 	"strings"
+	"sync"
+	"time"
 )
 
-// GithubRepos is github repositories root
-type GithubRepoSearchResult struct {
-	TotalCount int          `json:"total_count"`
-	Repos      []GithubRepo `json:"items"`
-	Incomplete bool         `json:"incomplete_results"`
-}
+var mu = &sync.Mutex{}
 
 // GithubRepo is github repository
 type GithubRepo struct {
-	//ID int `json:"id"`
-	Fork bool   `json:"fork"`
-	URL  string `json:"url"`
-	Desc string `json:"description"`
+	ID           int    `json:"id"`
+	Name         string `json:"name"`
+	IsFork       bool   `json:"fork"`
+	URL          string `json:"url"`
+	Desc         string `json:"description"`
+	LanguagesURL string `json:"languages_url"`
+	ContentsURL  string `json:"contents_url"`
+	GoImports    []string
 }
 
-// GithubItem is github content item
-type GithubItem struct {
-	Name string `json:"name"`
-	URL  string `json:"url"`
+func (g *GithubRepo) IsGo() bool {
+	var result bool
+
+	var langs map[string]int
+
+	rb, _ := GetRequestWithLimit(g.LanguagesURL)
+
+	err := json.Unmarshal(rb, &langs)
+	if err != nil {
+		return false
+	}
+
+	if _, ok := langs["Go"]; ok {
+		result = true
+	}
+
+	return result
+
+}
+
+func (g *GithubRepo) GetImports() {
+
+	gomodURL := strings.Replace(g.ContentsURL, "{+path}", "go.mod", 1)
+
+	gomodData, _ := GetRequestWithLimit(gomodURL)
+
+	if len(gomodData) > 0 {
+		g.GoImports = ParseGomodFile(gomodData)
+	}
 }
 
 func main() {
-	result := SearchGithubRepos("go")
-	fmt.Println(result)
+	result := GetGithubGoRepos()
+	fmt.Println(len(result))
+	f, _ := os.Create("data.out")
+	bw := bufio.NewWriter(f)
+	for _, repo := range result {
+		bw.WriteString(strings.Join([]string{repo.Name, strings.Join(repo.GoImports, ":"), "\n"}, "\t"))
+	}
+	bw.Flush()
 }
 
-// SearchGithubRepos searches for repos with specific lang
-func SearchGithubRepos(lang string) []GithubRepo {
-	var g []GithubRepo
-	u := url.URL{}
-	u.Scheme = "https"
-	u.Host = "api.github.com"
-	u.Path = "/search/repositories"
-	q := u.Query()
-	q.Set("q", "language:"+lang)
-	u.RawQuery = q.Encode()
-	//fmt.Println(u)
+// GetGithubGoRepos searches for repos with specific lang
+func GetGithubGoRepos() []GithubRepo {
+	var goRepos []GithubRepo
 
-	client := &http.Client{}
+	nextURL := "https://api.github.com/repositories"
 
-	req, err := http.NewRequest("GET", u.String(), nil)
-	if err != nil {
-		log.Fatal("Cant intitialize request:", err)
+	for {
+		var repos []GithubRepo
+
+		rb, lm := GetRequestWithLimit(nextURL)
+
+		err := json.Unmarshal(rb, &repos)
+		if err != nil {
+			log.Fatal("Error unmarshaling", err)
+		}
+
+		for _, repo := range repos {
+			fmt.Println("Processing:", repo.URL)
+			if repo.IsGo() && !repo.IsFork {
+				repo.GetImports()
+				fmt.Println("Go repo! Imports from gomod:\n", strings.Join(repo.GoImports, "\n"))
+				goRepos = append(goRepos, repo)
+			}
+		}
+
+		if val, ok := lm["since"]; ok {
+			nextURL = val
+		} else {
+			break
+		}
 	}
-	req.Header.Add("Accept", "application/vnd.github.mercy-preview+json")
-
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal("Error sending request:", err)
-	}
-
-	err = json.NewDecoder(resp.Body).Decode(&g)
-	if err != nil {
-		log.Fatal("Error unmarshaling", err)
-	}
-	return g
+	return goRepos
 }
 
+// ParseLinkHeader gets reference links from headers
 func ParseLinkHeader(s string) map[string]string {
 	links := make(map[string]string)
 
@@ -83,4 +121,63 @@ func ParseLinkHeader(s string) map[string]string {
 		}
 	}
 	return links
+}
+
+// GetRequestWithLimit is attemp to make simple rate limiter using sleep and mutex
+func GetRequestWithLimit(u string) ([]byte, map[string]string) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	time.Sleep(5 * time.Second)
+
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", u, nil)
+	if err != nil {
+		log.Fatal("Cant intitialize request:", err)
+	}
+	req.Header.Add("Accept", "application/vnd.github.mercy-preview+json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		resp.Body.Close()
+		log.Fatal("Error sending request:", err)
+	}
+	defer resp.Body.Close()
+
+	rb, _ := ioutil.ReadAll(resp.Body)
+	lh := resp.Header.Get("Link")
+	lm := ParseLinkHeader(lh)
+
+	return rb, lm
+}
+
+// ParseGomodFile get imports from gomod file
+func ParseGomodFile(b []byte) []string {
+	var goimports []string
+
+	bs := bytes.Split(b, []byte("\n"))
+
+	reStart := regexp.MustCompile(`^require \($`)
+	reEnd := regexp.MustCompile(`^\}$`)
+	reImport := regexp.MustCompile(`^(.*)\sv`)
+	reIndirect := regexp.MustCompile(`indirect`)
+
+loop:
+	for i := 0; i < len(bs); i++ {
+		if reStart.Match(bs[i]) {
+			for {
+				i++
+				importMatch := reImport.FindSubmatch(bytes.TrimSpace(bs[i]))
+				if len(importMatch) == 2 && !reIndirect.Match(bs[i]) {
+					goimports = append(goimports, string(importMatch[1]))
+				}
+
+				if reEnd.Match(bs[i]) {
+					break loop
+				}
+			}
+		}
+	}
+
+	return goimports
 }
--- main_test.go
+++ main_test.go
@@ -6,6 +6,27 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var gomodFile = `
+// This is a generated file. Do not edit directly.
+// Run hack/pin-dependency.sh to change pinned dependency versions.
+// Run hack/update-vendor.sh to update go.mod files and the vendor directory.
+
+module k8s.io/kubernetes
+
+go 1.13
+
+require (
+	bitbucket.org/bertimus9/systemstat v0.0.0-20180207000608-0eeff89b0690
+	github.com/Rican7/retry v0.1.0 // indirect
+
+}
+
+replace (
+	bitbucket.org/bertimus9/systemstat => bitbucket.org/bertimus9/systemstat v0.0.0-20180207000608-0eeff89b0690
+	cloud.google.com/go => cloud.google.com/go v0.38.0
+}
+`
+
 func TestParseLinkHeader(t *testing.T) {
 	testcase := struct {
 		in   string
@@ -19,5 +40,15 @@ func TestParseLinkHeader(t *testing.T) {
 	}
 	got := ParseLinkHeader(testcase.in)
 	assert.Equal(t, got, testcase.want, "Should be equal")
+
+}
+
+func TestParseGomod(t *testing.T) {
+	want := []string{
+		"bitbucket.org/bertimus9/systemstat",
+	}
+	got := ParseGomodFile([]byte(gomodFile))
+
+	assert.Equal(t, want, got, "Should be equal")
 
 }
