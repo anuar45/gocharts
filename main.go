5ee291d6b2c063e50e24084cf1c660f7b8f398d6
Anuar Serdaliyev <serdaliyev.anuar@gmail.com>
2020-03-03 17:56:06 +0100

    testing repository

--- main.go
+++ main.go
@@ -40,7 +40,7 @@ func main() {
 	f, _ := os.Create("data.out")
 	bw := bufio.NewWriter(f)
 	for i, v := range gis {
-		s := strconv.Itoa(i) + v.URL + v.Count
+		s := strconv.Itoa(i) + "  " + v.URL + "  " + strconv.Itoa(v.Count) + "\n"
 		bw.WriteString(s)
 	}
 	bw.Flush()
